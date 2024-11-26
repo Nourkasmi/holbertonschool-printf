ffdc08472acd76f8f984f93a48bf2e25cd8a0800
wwalid18 <9833@holbertonstudents.com>
2024-11-26 11:23:26 +0100

    starting fresh

--- _printf.c
+++ _printf.c
@@ -8,44 +8,4 @@
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char c, *str;
-va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-c = va_arg(args, int);
-count += write(1, &c, 1);
-}
-else if (*format == 's')
-{
-str = va_arg(args, char *);
-while (*str)
-{
-count += write(1, str++, 1);
-}
-}
-else if (*format == '%')
-{
-count += write(1, "%", 1);
-}
-else
-{
-count += write(1, "%", 1);
-count += write(1, format, 1);
-}
-}
-else
-{
-count += write(1, format, 1);
-}
-format++;
-}
-va_end(args);
-return (count);
 }

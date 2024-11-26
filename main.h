ee32a4b3d6e05cc00ee84866e546158efdf1b7e9
wwalid18 <9833@holbertonstudents.com>
2024-11-26 11:58:30 +0100

    structuring

--- main.h
+++ main.h
@@ -6,4 +6,19 @@
 
 int _printf(const char *format, ...);
 
+/**
+ * struct print_func - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @print: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct print_func
+{
+void (*print)(va_list args);
+char spec;
+} print_func;
+
+
+
 #endif

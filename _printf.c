3ed115f5705b0bea45247262c39dc6543afe05a6
wwalid18 <9833@holbertonstudents.com>
2024-11-28 11:27:22 +0100

    tsting lngth

--- _printf.c
+++ _printf.c
@@ -12,9 +12,10 @@ int i = 0, len = 0, j = 0;
 specifier_t spec_table[] = {
 {'c', print_c}, {'s', print_s}, {'i', print_i}, {'d', print_d}, {0, NULL}
 };
-va_start(args, format);
 if (format == NULL)
+printf ("we are heeree ");
 return (0);
+va_start(args, format);
 while (format[i])
 {
 if (format[i] == '%' && format[i + 1] == '%')
